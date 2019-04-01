3f17b02ea1d3cd06321e2ab54ca5bc6b669de3de
DanielFirmo <49136060+DanielFirmo@users.noreply.github.com>
2019-03-31 23:34:43 -0300

    Create Vertice.java

---
+++ Vertice.java
@@ -0,0 +1,35 @@
+
+public class Aresta {
+	private Vertice origem;
+	private Vertice destino;
+	private boolean segue;
+
+	public Vertice getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(Vertice origem) {
+		this.origem = origem;
+	}
+
+	public Vertice getDestino() {
+		return destino;
+	}
+
+	public void setDestino(Vertice destino) {
+		this.destino = destino;
+	}
+
+	public boolean getSegue() {
+		return segue;
+	}
+
+	public void setSegue(boolean segue) {
+		this.segue = segue;
+	}
+
+}
+
+	
+
+
